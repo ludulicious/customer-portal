ca188a3af0bf949c68de5c7f14e00b816393e35e
Marcel Posdijk <mposdijk@marpos.nl>
2025-10-28 13:24:34 +0100

    prisma multiple schema files setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,20 @@
-import path from 'node:path'
-import type { PrismaConfig } from 'prisma'
+import path from "node:path";
+import type { PrismaConfig } from "prisma";
 
 export default {
-  schema: path.join('prisma'),
-} satisfies PrismaConfig
+  schema: path.join("prisma", "schema", "schema.prisma"),
+  migrations: {
+    path: path.join("prisma", "migrations"),
+  },
+  views: {
+    path: path.join("prisma", "schema", "views"),
+  },
+  typedSql: {
+    path: path.join("prisma", "schema", "queries"),
+  },
+  engine: "classic",
+  datasource: {
+    url: process.env.DATABASE_URL ||
+      'postgresql://postgres:postgres@localhost:5432/postgres'
+  }
+} satisfies PrismaConfig;
