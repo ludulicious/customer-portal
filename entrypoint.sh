1aae1b232dfa9a12631b3af7973c3cdc369f621b
Marcel Posdijk <mposdijk@marpos.nl>
2025-10-29 15:03:42 +0100

    fix entrypint.sh without migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,15 @@
 set -e
 
 # Run Prisma DB migrate deploy. Requires DATABASE_URL to be set.
-echo "Running Prisma DB migrate deploy..."
-npx prisma migrate deploy
+# echo "Running Prisma DB migrate deploy..."
+# npx prisma migrate deploy
+
+# # Execute the command passed as arguments to this script (which is the CMD from Dockerfile)
+# echo "Starting Nuxt server..."
+# exec "$@"
+#!/bin/sh
+# Exit immediately if a command exits with a non-zero status.
+# set -e
 
 # Execute the command passed as arguments to this script (which is the CMD from Dockerfile)
 echo "Starting Nuxt server..."
