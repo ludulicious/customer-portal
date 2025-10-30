41a8a929a41c39d8c0462450af3fba22553f08b4
Marcel Posdijk <mposdijk@marpos.nl>
2025-10-30 13:26:32 +0100

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,27 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt, { defineFlatConfigs } from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-  {
-    rules: {
-      'comma-dangle': 'off',
-      'semi': ['error', 'never'],
-      'vue/html-indent': 'off',
-      '@stylistic/comma-dangle': 'off',
-      '@stylistic/no-trailing-spaces': 'off',
-      'vue/max-attributes-per-line': 'off',
-      'vue/block-tag-newline': 'off',
-      'vue/html-closing-bracket-newline': 'off',
-      'vue/html-closing-bracket-spacing': 'off',
-      'vue/html-comment-content-newline': 'off',
-      'vue/html-comment-content-spacing': 'off',
-      'vue/html-comment-indent': 'off',
-      'vue/html-end-tags': 'off',
-      'vue/first-attribute-linebreak': 'off',
-      'nuxt/nuxt-config-keys-order': 'off',
+export default defineFlatConfigs(
+  withNuxt(
+    // Your custom configs here
+    {
+      rules: {
+        'comma-dangle': 'off',
+        'semi': ['error', 'never'],
+        'vue/html-indent': 'off',
+        '@stylistic/comma-dangle': 'off',
+        '@stylistic/no-trailing-spaces': 'off',
+        'vue/max-attributes-per-line': 'off',
+        'vue/block-tag-newline': 'off',
+        'vue/html-closing-bracket-newline': 'off',
+        'vue/html-closing-bracket-spacing': 'off',
+        'vue/html-comment-content-newline': 'off',
+        'vue/html-comment-content-spacing': 'off',
+        'vue/html-comment-indent': 'off',
+        'vue/html-end-tags': 'off',
+        'vue/first-attribute-linebreak': 'off',
+        'nuxt/nuxt-config-keys-order': 'off',
+      }
     }
-  }
+  )
 )
